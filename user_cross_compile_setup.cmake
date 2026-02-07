ce526d6c0a4338ba8637bcc985cbe844641d4dba
air jinkela <air_jinkela@163.com>
2026-02-07 19:43:21 +0800

    force set find libs in toolchain dir and target dir
    Signed-off-by: air jinkela <air_jinkela@163.com>

--- user_cross_compile_setup.cmake
+++ user_cross_compile_setup.cmake
@@ -3,13 +3,30 @@
 # make  -C build -j
 
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(tools /home/jkl/openwrts/rjcommunity/MT798X-6.6-24.10-workflow/staging_dir/toolchain-aarch64_cortex-a53_gcc-13.3.0_musl)
-set(CMAKE_C_COMPILER ${tools}/bin/aarch64-openwrt-linux-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-openwrt-linux-g++)
+set(TOOLCHAIN_DIR /home/jkl/openwrts/rjcommunity/MT798X-6.6-24.10-workflow/staging_dir/toolchain-aarch64_cortex-a53_gcc-13.3.0_musl)
+set(TARGET_DIR    /home/jkl/openwrts/rjcommunity/MT798X-6.6-24.10-workflow/staging_dir/target-aarch64_cortex-a53_musl)
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-openwrt-linux-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/aarch64-openwrt-linux-g++)
+
+set(CMAKE_SYSROOT ${TARGET_DIR})
+set(CMAKE_FIND_ROOT_PATH
+    ${TOOLCHAIN_DIR}
+    ${TARGET_DIR}
+)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(ENV{PKG_CONFIG_SYSROOT_DIR} ${TARGET_DIR})
+set(ENV{PKG_CONFIG_PATH}
+    "${TARGET_DIR}/usr/lib/pkgconfig:${TARGET_DIR}/usr/share/pkgconfig"
+)
 
 # If necessary, set STAGING_DIR
 # if not work, please try(in shell command): export STAGING_DIR=/home/ubuntu/Your_SDK/out/xxx/openwrt/staging_dir/target
-set(ENV{STAGING_DIR} "/home/jkl/openwrts/rjcommunity/MT798X-6.6-24.10-workflow/staging_dir/target-aarch64_cortex-a53_musl")
-
+set(ENV{STAGING_DIR} ${TARGET_DIR})
